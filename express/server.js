57f1654c17f5e034c6212e4ef56a46d4c74ae09c
devpola <68686204+devpola@users.noreply.github.com>
2020-12-05 16:31:38 +0900

    express 소스 추가(login 후, main 페이지 이동)

---
+++ express/server.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const app = express();
+const multer = require('multer');
+const upload = multer();
+const fs = require('fs');
+
+const database = {
+    users: [
+        {
+            name: 'jiykim',
+            password: 'A!!003697jiykim'
+        }
+    ]
+};
+
+//middleware
+app.use(express.static(__dirname + '/public'));
+
+app.get('/signin', (req, res) => {
+    fs.readFile('public/signin.html', (error, data) => {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.end(data);
+    });
+});
+
+app.post('/login', upload.none(), (req, res) => {
+    console.log("login!!");
+    var userName = req.body.name;
+    var password = req.body.password;
+    if(userName ===  database.users[0].name && password === database.users[0].password){
+        console.log("success");
+        //if success on signin, render dashboard
+        fs.readFile('public/dashboard.html', (error, data) => {
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.end(data);
+        });     
+    }
+    else{
+        console.log("false!");
+    }
+});
+
+app.listen(3000, () => {
+    console.log("app is running on port 3000");
+});
