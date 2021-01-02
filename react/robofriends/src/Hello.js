eb4873ac47b7f2e11bd1af0f401da9add9e4e33b
devpola <68686204+devpola@users.noreply.github.com>
2021-01-02 15:12:52 +0900

    Add Hello component

---
+++ react/robofriends/src/Hello.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Hello.css';
+
+class Hello extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>hello world!</h1>
+                <p>Welcome!!</p>
+            </div>
+        );
+    }
+}
+
+export default Hello;
