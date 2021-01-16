1ab8f2f496d8aebd0ecc5b1556f16eff6d653a20
devpola <68686204+devpola@users.noreply.github.com>
2021-01-16 23:41:50 +0900

    node.js module 실습

---
+++ node/script.mjs
@@ -0,0 +1,9 @@
+//const mod = require('./module.js');
+import {largeNumber} from './es6module.mjs';
+
+//const a = mod.largeNumber;
+const b = 6;
+const c = largeNumber;
+
+//console.log(a + b);
+console.log(b + c);
