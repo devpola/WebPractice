3a9cbaa14519912feeb3fa625c8585d828382d5c
devpola <68686204+devpola@users.noreply.github.com>
2021-01-18 23:31:17 +0900

    node.js RESTful APIs 및 file system module 학습

--- node/server.js
+++ node/server.js
@@ -1,15 +1,45 @@
-const http = require('http');
+const express = require('express');
 
-const server = http.createServer((request, response) => {
-    //console.log('headers',request.headers);
-    console.log('method',request.method);
-    console.log('url',request.url);
-    const user = {
-        name: 'John',
-        hobby: 'Skating'
-    }
-    response.setHeader('Content-Type', 'application/json');
-    response.end(JSON.stringify(user));
+const app = express();
+
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+    console.log(req.query);
+    //console.log(req.body);
+    //console.log(req.header);
+    res.send('getting root');
 });
 
-server.listen(3000);
+app.get('/:id', (req, res) => {
+    console.log(req.params);
+    res.send('getting id');
+});
+
+
+app.listen(3000);
+
+
+
+
+
+
+
+
+// const http = require('http');
+
+// const server = http.createServer((request, response) => {
+//     //console.log('headers',request.headers);
+//     console.log('method',request.method);
+//     console.log('url',request.url);
+//     const user = {
+//         name: 'John',
+//         hobby: 'Skating'
+//     }
+//     response.setHeader('Content-Type', 'application/json');
+//     response.end(JSON.stringify(user));
+// });
+
+// server.listen(3000);
